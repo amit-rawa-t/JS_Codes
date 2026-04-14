4a234e17ccc22caadae4d8d81f2f28345a80161c
amit-rawa-t <amitrawat2025@gmail.com>
2026-04-14 16:44:48 +0000

    Conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let islogged = 15;
+
+let booleanislogged = Boolean(islogged);
+console.log(booleanislogged);
+
+// 1 => true; 0 => false;
+// "" => fasle;
+// "Amit" => true;
+// 10,14,32 => true;
+
+
+// same goes for String(any string)
+// number will be converted to string type  
