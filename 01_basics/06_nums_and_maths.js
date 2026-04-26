453c6bb373a218b1500a24bbaacb55ba31475c3e
amit-rawa-t <amitrawat2025@gmail.com>
2026-04-26 16:35:34 +0000

    Learned about string and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 600;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernumber = 123.8966;
+
+// console.log(othernumber.toPrecision(3));
+
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++ Maths +++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.2));
+
+console.log(Math.random());
+console.log((Math.random()* 10) + 1);
+
